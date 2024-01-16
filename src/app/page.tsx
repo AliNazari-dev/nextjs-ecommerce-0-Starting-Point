4b1da7ec98ec2f8b91648b33095f40e311eced2c
Ali <AliNazarii7394@gmail.com>
2024-01-16 10:54:52 +0330

    Home metadata dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,43 @@
 import ProductCart from "@/components/ProductCart";
 import { prisma } from "@/lib/db/prisma";
 import Image from "next/image";
+import Link from "next/link";
 
 export default async function Home() {
-  const product = await prisma.product.findMany({
+  const products = await prisma.product.findMany({
     orderBy: {
       id: "desc",
     },
   });
   return (
     <div>
-      <ProductCart product={product[2]} />
+      <div className="hero rounded-xl bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+          <Image
+            src={products[0].image}
+            alt={products[0].name}
+            width={800}
+            height={400}
+            className="w-full max-w-sm rounded-lg shadow-xl"
+            priority
+          />
+          <div>
+            <h1 className="text-5xl font-bold">{products[0].name}</h1>
+            <p className="py-6">{products[0].description}</p>
+            <Link
+              href={`/products/${products[0].id}`}
+              className="btn-primary btn"
+            >
+              Check it now
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="my-4 grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
+        {products.slice(1).map((product) => (
+          <ProductCart key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/app/products/[id]/page.tsx
@@ -0,0 +1,60 @@
+import { metadata } from "@/app/layout";
+import PriceTag from "@/components/PriceTag";
+import { prisma } from "@/lib/db/prisma";
+import { Metadata } from "next";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import React, { cache } from "react";
+
+interface ProductPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const getProducts = cache(async (id: string) => {
+  const product = await prisma.product.findUnique({
+    where: { id },
+  });
+
+  if (!product) notFound();
+
+  return product;
+});
+
+export async function generateMetadata({
+  params: { id },
+}: ProductPageProps): Promise<Metadata> {
+  const product = await getProducts(id);
+
+  return {
+    title: product.name + " - alimazon",
+    description: product.description,
+    openGraph: {
+      images: [{ url: product.image }],
+    },
+  };
+}
+
+const ProductPage = async ({ params: { id } }: ProductPageProps) => {
+  const product = await getProducts(id);
+  return (
+    <div className="flex flex-col items-center gap-5 lg:flex-row">
+      <Image
+        alt={product.name}
+        src={product.image}
+        width={500}
+        height={500}
+        className="rounded-lg"
+        priority
+      />
+      <div>
+        <h1 className="text-5xl font-bold">{product.name}</h1>
+        <PriceTag price={product.price} className="mt-4" />
+        <p className="py-6">{product.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductPage;
