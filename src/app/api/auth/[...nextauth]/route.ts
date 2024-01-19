c15a7d92fd3725ab53e0bc5323e504194e1c4929
Ali <AliNazarii7394@gmail.com>
2024-01-19 17:41:24 +0330

    next-auth added to project

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,21 @@
+import { prisma } from "@/lib/db/prisma";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
+import { Adapter } from "next-auth/adapters";
+import NextAuth from "next-auth/next";
+import { env } from "@/lib/env";
+
+export const authOption: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+};
+
+const handler = NextAuth(authOption);
+
+export { handler as GET, handler as POST };
